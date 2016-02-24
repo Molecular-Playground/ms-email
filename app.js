bd5fc4d85de7a5b2af6c62e2e5adc256afb77abd
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-02-23 22:43:36 -0500

    Added 2nd route to email.  Changed main email route to 'mailer.' /verify checks sets specific messages and checks for specific req.body properties, /general checks only general props.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/send', send);
+app.use('/mailer', send);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/send.js
+++ routes/send.js
@@ -21,13 +21,13 @@ var mailOptions = {
 
 
 // PUT: send the email!
-router.put('/', function(req, res, next) {
+router.put('/validate', function(req, res, next) {
+  mailOptions.text = "Hi there!\nWe're so glad you've joined.  Follow the link below to verify your account.\nThanks!\nThe Molecular Playground Team";
+  mailOptions.html = "<p>Hi there!</p><p>We so glad you've joined. Follow the link below to verify your account.</p><p>Thanks!</p><p>The Molecular Playground Team</p>";
+  mailOptions.subject = 'Welcome to Molecular Playground!';
   if (req.body.email) {
     mailOptions.to = req.body.email;
   }
-  if(req.body.subject) mailOptions.subject = req.body.subject;
-  if(req.body.text) mailOptions.text = req.body.text;
-  if(req.body.html) mailOptions.html = req.body.html;
   if (req.body.link) {
     mailOptions.text += "\n Your link: " + req.body.link;
     mailOptions.html += "<p> Your link: " + req.body.link + "</p>";
@@ -43,5 +43,30 @@ router.put('/', function(req, res, next) {
     }
   });
 });
+
+
+router.put('/general', function(req, res, next) {
+  if (req.body.email) {
+    mailOptions.to = req.body.email;
+  }
+  if(req.body.subject) mailOptions.subject = req.body.subject;
+  if(req.body.text) {
+    mailOptions.text = req.body.text;
+  }
+  if(req.body.html) mailOptions.html = req.body.html;
+  console.log(req.body);
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } 
+    else {
+      console.log('Message sent: ' + info.response);
+    }
+  });
+});
+
+
+
 
 module.exports = router;
